8677f289526b35fc5c74aa77690ad7e38774a308
Aleksander Aleksievich <aleksievisa@gmail.com>
2022-11-10 02:04:31 +0300

    start

---
+++ src/ormconfig.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { ConnectionOptions } from 'typeorm';
+
+dotenv.config({
+  path: path.join(__dirname, '.env'),
+});
+
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST } = process.env;
+const LOCAL_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
+
+export default {
+  type: 'postgres',
+  cache: false,
+  url: 'postgres://alex99like:g1cRAOO4cA0wrTmwLpnU093wchot9hot@dpg-cdm2fv82i3mrtiv6t2ug-a/management_n927',
+  synchronize: true,
+  logging: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: ['src/resources/**/**.entity{.ts,.js}'],
+  migrations: ['./migrations/*.ts'],
+} as ConnectionOptions;
